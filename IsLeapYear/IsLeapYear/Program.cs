b2ee9c4102b0c226ba0ba4521bb77e6abab5ff56
Evgen <81950721+GuitaristForm@users.noreply.github.com>
2022-01-05 21:10:55 +0300

    init

--- IsLeapYear/IsLeapYear/Program.cs
+++ IsLeapYear/IsLeapYear/Program.cs
@@ -1,7 +1,16 @@
 ﻿class solution
 {
-    public static void main()
+    public static bool IsLeap(int Year)
     {
-
+        if(Year % 4 == 0 || Year % 100 != 0 && Year % 400 == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+    public static void Main()
+    {
+        int Year = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(IsLeap(Year));
     }
 }
