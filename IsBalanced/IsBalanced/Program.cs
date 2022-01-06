2a9eb824fd52302fb7b87a9324545cf16a9ec20f
Evgen <81950721+GuitaristForm@users.noreply.github.com>
2022-01-06 14:38:57 +0300

    Balance ~(*_*)~

---
+++ IsBalanced/IsBalanced/Program.cs
@@ -0,0 +1,36 @@
+﻿class Solution
+{
+    public static void Main()
+    {
+        string Str = Console.ReadLine();
+
+        Console.WriteLine(IsBalanced(Str));
+    }
+    public static bool IsBalanced(string Str)
+    {
+        Stack<char> Brackets = new Stack<char>();
+
+        for (int i = 0; i < Str.Length; i++)
+        {
+            if (Str[i] == '(' || 
+                Str[i] == '[' || 
+                Str[i] == '{')
+            {
+                Brackets.Push(Str[i]);
+            }
+            else
+            {
+                if (Brackets.Count() < 1 ||
+                   (Brackets.Peek() == '(' && Str[i] != ')') ||
+                   (Brackets.Peek() == '[' && Str[i] != ']') ||
+                   (Brackets.Peek() == '{' && Str[i] != '}'))
+                {
+                    return false;
+                }
+                Brackets.Pop();
+            }
+        }
+
+        return Brackets.Count() == 0;
+    }
+}
