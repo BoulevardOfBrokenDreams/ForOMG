2fe05b89e92b5b914fcbf8ed7f66b35829b1508e
Evgen <81950721+GuitaristForm@users.noreply.github.com>
2022-01-06 13:34:26 +0300

    Guns'n'Roses

--- Palindrome/Palindrome/Program.cs
+++ Palindrome/Palindrome/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿class Solution
+{
+    public static bool IsPalindrome(string Str)
+    {
+        int LenStr = Str.Length - 1;
+
+        for(int i = 0; i < LenStr / 2; i++)
+        {
+            if(Str[i] != Str[LenStr - i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void Main()
+    {
+        string Str = Console.ReadLine();
+        for(int i = 0; i < Str.Length; i++)
+        {
+            if (char.IsPunctuation(Str[i]) || Str[i] == ' ')
+            {
+                Str = Str.Remove(i,1);
+            }
+        }
+        Console.WriteLine(IsPalindrome(Str.ToLower()));
+    }
+}
