3dc386a34e0b991f9ea92621191613d6f9b8e927
Evgen <81950721+GuitaristForm@users.noreply.github.com>
2022-01-18 20:41:11 +0300

    Fix Task 1

--- IsLeapYear/IsLeapYear/Program.cs
+++ IsLeapYear/IsLeapYear/Program.cs
@@ -2,7 +2,7 @@
 {
     public static bool IsLeap(int Year)
     {
-        return Year % 4 == 0 || Year % 100 != 0 && Year % 400 == 0;
+        return (Year % 4 == 0 || Year % 100 != 0) && Year % 400 == 0;
     }
     public static void Main()
     {
